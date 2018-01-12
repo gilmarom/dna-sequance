992e95e87c83ebcf05a0a7669a17fc46defbf0af
gil <gilmarom23@gmail.com>
2018-01-12 15:43:38 -0800

    update version

--- routes/index.js
+++ routes/index.js
@@ -13,38 +13,28 @@ router.get('/', function(req, res, next){
 router.post('/send', function (req, res, next) {
     
     var data = req.body.data;
-    
-    console.log(data ," data");
     var spawn = require('child_process').spawn,
     py = spawn('python', ['./api/writeDna.py']),
-             
     dataString = '';
-    
     py.stdout.on('data', function(data){
        dataString += data.toString();
        console.log((dataString.replace("'",'"')), " dataString");
     });
             
        py.stdout.on('end', function(){
-       console.log(dataString ," check");
-     
        dna = JSON.parse(dataString.replace(/'/g,'"'));
-       console.log(dna);
-       //console.log(typeof(dna));
-       console.log(dna, "1");
        res.redirect('/');   
     });
      
     py.stdin.write(JSON.stringify(data));
     py.stdin.end();
   
-    console.log(dna , " json");
+    
      
 });
 
 router.post('/aminoAcidPrecentage', function(req,res,next){
-    console.log(req.body.data);   
-    console.log(data ," data");
+   
     var spawn = require('child_process').spawn,
     py = spawn('python', ['./api/aminoAcid.py']),
     data = [req.body.data],         
@@ -52,14 +42,14 @@ router.post('/aminoAcidPrecentage', function(req,res,next){
     
     py.stdout.on('data', function(data){
        dataString += data.toString();
-       console.log((dataString.replace("'",'"')), " dataString");
+      
     });
             
     py.stdout.on('end', function(){
-    console.log(dataString);
+   
      
     aa = JSON.parse(dataString.replace(/'/g,'"'));
-    console.log(aa, "aa");
+
     //console.log(typeof(dna));
     
     res.redirect('/');   
